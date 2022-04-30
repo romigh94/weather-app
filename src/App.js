f5e8ed122f402ebd49a16c0a66983db7615edc37
Romina Ghaderi <romina.ghaderi@outlook.com>
2022-04-30 13:15:49 +0200

    adding background, design and divs

--- src/App.js
+++ src/App.js
@@ -1,23 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+
+const api = {
+  key: "bf14ea7eee34efe82391c805dbbc9500",
+  base: "https://api.openweathermap.org/data/2.5"
+}
 
 function App() {
+
+  const dateToday = () => {
+      let newDate = new Date()
+      let date = newDate.getDate();
+      let month = newDate.getMonth() + 1;
+      let year = newDate.getFullYear();
+      let separator = "-";
+
+      return `${year}${separator}${month<10?`0${month}`:`${month}`}${separator}${date}`
+  }
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+      <main>
+        <div className="search-box">
+            <input type="text" className="searchbar" placeholder="Sök..." />
+        </div>
+        <div className="location-box">
+            <div className="location">Stockholm, SE</div>
+            <div className="date">{dateToday()}</div>
+        </div>
+        <div className="weather-box">
+          <div className="temp">13ºc</div>
+          <div className="weather">Soligt</div>
+        </div>
+      </main>
+
     </div>
   );
 }
